2af6483d71db5414f8257001f2ace23a51c97e4d
Mevlüt Yıldırım <mevlut.yildirim84@erzurum.edu.tr>
2020-10-19 12:49:48 +0300

    NewsApi-Kullanim

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask,render_template
+from newsapi import NewsApiClient
+
+app = Flask(__name__)
+
+@app.route('/')
+def Index():
+    newsapi = NewsApiClient(api_key="a0cab91e217e4bb4bf82d3b138aa1a18")
+    topheadlines = newsapi.get_top_headlines(country="tr")
+
+    articles = topheadlines['articles']
+
+    desc = []
+    news = []
+    img = []
+    content = []
+    url =[]
+    date = []
+
+    for i in range(len(articles)):
+        myarticles = articles[i]
+
+        news.append(myarticles['title'])
+        desc.append(myarticles['description'])
+        img.append(myarticles['urlToImage'])
+        content.append(myarticles['content'])
+        url.append(myarticles['url'])
+        date.append(myarticles['publishedAt'])
+    mylist = zip(news,desc,img,content,url,date)
+
+    return render_template('index.html',context = mylist)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
